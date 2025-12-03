3cae8d239ef4cf58d431041756e9224081fe8db1
Puvish <p.kungsapattarakul@gmail.com>
2025-12-03 21:10:15 +0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import * as Tone from 'tone';
+	const synth = new Tone.Synth().toDestination();
+</script>
+
+<button
+	on:click={(e) => {
+		synth.triggerAttackRelease('C4', '8n');
+	}}>Play</button
+>
